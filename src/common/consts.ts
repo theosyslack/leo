8bd3cb8578ed9f163a5bdd51d8475907760ed635
Theo Syslack <tsyslack@crescendocollective.com>
2019-09-11 21:08:29 -0500

    move db outside of dist

--- src/common/consts.ts
+++ src/common/consts.ts
@@ -1,1 +1,5 @@
-export const JIRA_HISTORY_FILE_PATH = __dirname + "/db/jira/history.json";
+import * as path from "path";
+export const JIRA_HISTORY_FILE_PATH = path.resolve(
+  __dirname,
+  "../.db/jira/history.json"
+);
